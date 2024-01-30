b5ff25f0122ee7c503115bf5f7fbf9feefb519eb
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-01-30 20:56:43 +0530

    done

---
+++ calc/src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ACTIONS } from '../App'
+function Button({dispatch, digit}) {
+  return (
+    <div>
+        <button onClick={()=>dispatch({type:ACTIONS.ADD_DIGIT, payload: {digit}})}>{digit}</button>
+    </div>
+  )
+}
+
+export default Button
